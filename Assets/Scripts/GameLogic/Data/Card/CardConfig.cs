5f3d21bc4077a19855a511b76d13c717ce3b2aee
chendingming <429545632@qq.com>
2022-07-13 10:19:46 +0800

    UI

--- Assets/Scripts/GameLogic/Data/Card/CardConfig.cs
+++ Assets/Scripts/GameLogic/Data/Card/CardConfig.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class CardType
+{
+    public const string None = "none";
+    public const string Search = "search";
+    public const string Move = "move";
+    public const string Leader = "lead";
+}
+
 public class CardConfig : NSConfigObject
 {
     public string name { get; private set; }
     public float cost { get; private set; }
     public float value { get; private set; }
+    public string cardType { get; private set; }
 
     public override void initializeByParameters(Dictionary<string, object> parameters)
     {
@@ -14,5 +23,6 @@ public class CardConfig : NSConfigObject
         this.name = parameters.stringValue("name");
         this.cost = parameters.floatValue("cost");
         this.value = parameters.floatValue("value");
+        this.cardType = parameters.stringValue("cardType", CardType.None);
     }
 }
